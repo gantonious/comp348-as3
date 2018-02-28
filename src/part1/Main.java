cb110d27e0265b89746f470580f8c33c5c53cc7e
George Antonious <george@antonious.ca>
2018-02-28 13:51:05 -0700

    Add java docs to main files

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -4,7 +4,23 @@ import part1.poems.PoemService;
 import part1.server.PoemServer;
 
 /**
- * Created by George on 2017-12-29.
+ * title: Main.java
+ * description: The main entry for the Poem of the Day server.
+ * date: February 28, 2018
+ * @author George Antonious
+ * @version 1.0
+ * @copyright 2018 George Antonious
+ *
+ * I declare that this assignment is my own work and that all material
+ * previously written or published in any source by any other person
+ * has been duly acknowledged in the assignment. I have not submitted
+ * this work, or a significant part thereof, previously as part of any
+ * academic program. In submitting this assignment I give permission to
+ * copy it for assessment purposes only.
+ *
+ * The usage, design, and test plan for this part can be found in the
+ * README.md file in the root of this project. It is recommended to view
+ * it in a markdown reader.
  */
 public class Main {
     public static void main(String[] args) {
--- src/part2/server/RemoteTaskExecutor.java
+++ src/part2/server/RemoteTaskExecutor.java
@@ -8,7 +8,23 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
- * Created by George on 2018-02-19.
+ * title: RemoteTaskExecutor.java
+ * description: The main entry remote method execution server.
+ * date: February 28, 2018
+ * @author George Antonious
+ * @version 1.0
+ * @copyright 2018 George Antonious
+ *
+ * I declare that this assignment is my own work and that all material
+ * previously written or published in any source by any other person
+ * has been duly acknowledged in the assignment. I have not submitted
+ * this work, or a significant part thereof, previously as part of any
+ * academic program. In submitting this assignment I give permission to
+ * copy it for assessment purposes only.
+ *
+ * The usage, design, and test plan for this part can be found in the
+ * README.md file in the root of this project. It is recommended to view
+ * it in a markdown reader.
  */
 public class RemoteTaskExecutor implements TaskExecutor {
 
